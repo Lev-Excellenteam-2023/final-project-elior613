e780a63fd082f892684a29e1cf2026f896a78145
elior613 <eliortk@gmail.com>
2023-05-23 22:07:42 +0300

    insert parse tables and images

--- Parse_the_presentation.py
+++ Parse_the_presentation.py
@@ -1,4 +1,48 @@
 from pptx import Presentation
+from io import BytesIO
+from PIL import Image
+
+def extract_content_from_shape(shape):
+    """
+    Extracts content from a shape based on its type (image or table).
+    """
+    content = ""
+
+    if shape.shape_type == 13:  # Check if the shape is an image (shape_type=13)
+        content += extract_image_content(shape)+" "
+    elif shape.shape_type == 19:  # Check if the shape is a table (shape_type=19)
+        content += extract_table_content(shape)+" "
+    else:
+        content += extract_text_from_shape(shape)+" "
+
+    return content.strip()
+
+def extract_image_content(shape):
+    """
+    Extracts and returns the content of an embedded image shape.
+    """
+    image = shape.image
+    image_data = image.blob
+    image_file = BytesIO(image_data)
+    image_pil = Image.open(image_file)
+    # Process the image and extract relevant content (e.g., using computer vision techniques)
+    # Replace the following line with your custom image processing code
+    content = f"[IMAGE CONTENT: {image.filename}]"
+    return content
+
+def extract_table_content(shape):
+    """
+    Extracts the contents of a table shape.
+    """
+    content = ""
+    table = shape.table
+
+    for row in table.rows:
+        for cell in row.cells:
+            cell_text = extract_text_from_shape(cell)
+            content += cell_text + " "
+
+    return content.strip()
 
 def extract_text_from_shape(shape, title_text=None):
     """
@@ -13,45 +57,45 @@ def extract_text_from_shape(shape, title_text=None):
     return text.strip()
 
 
-
 def extract_content_from_slide(slide):
     """
-    Extracts the title and content from a slide.
+    Extracts the title and content from a slide, including images and tables.
     """
     title = slide.shapes.title.text if slide.shapes.title else "NONE"
     content = ""
 
     for shape in slide.shapes:
-        if shape.shape_type == 13:  # Check if the shape is an image (shape_type=13)
-            content += "[IMAGE] "
-        else:
-            content += extract_text_from_shape(shape, title)+" "
+        content += extract_text_from_shape(shape, title)+" "
 
     return title, content
 
 
-
 def process_presentation(presentation_path):
     """
-    Processes a PowerPoint presentation and returns a dictionary with slide numbers as keys and tuples of title and content as values.
+    Processes a PowerPoint presentation and returns a list of dictionaries containing slide information.
     """
     presentation = Presentation(presentation_path)
-    slide_data = {}
+    slides_data = []
 
     for slide_num, slide in enumerate(presentation.slides, start=1):
         title, content = extract_content_from_slide(slide)
-        slide_data[slide_num] = (title, content)
+        slide_data = {
+            "slide_number": slide_num,
+            "title": title,
+            "content": content,
+        }
+        slides_data.append(slide_data)
 
-    return slide_data
+    return slides_data
 
-def print_slide_content(slide_data):
+def print_slide_content(slides_data):
     """
-    Prints the slide number, title, and content of slides.
+    Prints the slide number, title, content, and image subjects of slides.
     """
-    for slide_num, (title, content) in slide_data.items():
-        print(f"Slide {slide_num}")
-        print(f"Title: {title}")
-        print(f"Content: {content}")
+    for slide_data in slides_data:
+        print(f"Slide {slide_data['slide_number']}")
+        print(f"Title: {slide_data['title']}")
+        print(f"Content: {slide_data['content']}")
         print()
 
 # Usage example
